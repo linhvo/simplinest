f09990258ce018048dc1f3b50dc085f063607ae8
Linh Vo <lvo@umbel.com>
2016-08-25 00:00:05 -0500

    set thermostate to home

--- core/management/commands/set_to_home.py
+++ core/management/commands/set_to_home.py
@@ -10,6 +10,18 @@ class Command(BaseCommand):
     help = 'Set thermostate to home'
     def handle(self, *args, **options):
         auth = NestAuth.objects.first()
+        if not auth.access_token:
+            access_token = self.get_access_token(auth)
+            auth.access_token = access_token
+            auth.save()
+
+        url = "https://developer-api.nest.com/structures/g-9y-2xkHpBh1MGkVaqXOGJiKOB9MkoW1hhYyQk2vAunCK8a731jbg/away?auth=<AUTH_TOKEN>"
+        # curl - v - L - X
+        # PUT
+        # "https://developer-api.nest.com/structures/g-9y-2xkHpBh1MGkVaqXOGJiKOB9MkoW1hhYyQk2vAunCK8a731jbg/away?auth=<AUTH_TOKEN>" - H
+        # "Content-Type: application/json" - d
+        # '"away"'
+    def get_access_token(self, auth):
         auth_code = auth.auth_code
         client_id = auth.client_id
         client_secret = auth.client_secret
@@ -18,12 +30,4 @@ class Command(BaseCommand):
                             '&grant_type=authorization_code' % (client_id, auth_code, client_secret)
 
         resp = requests.post(request_token_url)
-        content = resp.json()['access_token']
-        auth.access_token = content['access_token']
-        auth.save()
-        url = "https://developer-api.nest.com/structures/g-9y-2xkHpBh1MGkVaqXOGJiKOB9MkoW1hhYyQk2vAunCK8a731jbg/away?auth=<AUTH_TOKEN>"
-        # curl - v - L - X
-        # PUT
-        # "https://developer-api.nest.com/structures/g-9y-2xkHpBh1MGkVaqXOGJiKOB9MkoW1hhYyQk2vAunCK8a731jbg/away?auth=<AUTH_TOKEN>" - H
-        # "Content-Type: application/json" - d
-        # '"away"'
+        return resp.json()['access_token']
