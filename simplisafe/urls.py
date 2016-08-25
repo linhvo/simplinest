a8715a077a2b88aa0c6b3fadd24349823c491137
Linh Vo <lvo@umbel.com>
2016-08-25 14:25:53 -0500

    update url

--- simplisafe/urls.py
+++ simplisafe/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
-
 from django.contrib import admin
+import core.views
 
 admin.autodiscover()
 
-urlpatterns = [url(r'^$', 'core.views.home', name='home'),
+urlpatterns = [url(r'^$', core.views.home, name='home'),
                url(r'^admin/', include(admin.site.urls)),
-               url(r'^simplisafe/away', 'core.views.simplisafe_away', name='simplisafe_away'),
+               url(r'^simplisafe/away', core.views.simplisafe_away, name='simplisafe_away'),
                ]
