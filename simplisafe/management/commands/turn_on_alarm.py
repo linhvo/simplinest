bd212fe94026b19827af038966d8799cfb8e77d0
Linh Vo <linh.h.vo@ibm.com@linhs-mbp.attlocal.net>
2018-03-28 23:00:34 -0500

    add log

--- simplisafe/management/commands/turn_on_alarm.py
+++ simplisafe/management/commands/turn_on_alarm.py
@@ -10,5 +10,6 @@ class Command(BaseCommand):
     help = 'Turn on alarm at 11:30'
 
     def handle(self, *args, **options):
+        print('Turning on alarm')
         res = set_simplisafe_state('home')
         print(res.json())
