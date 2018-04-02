1db9de7ab0efd3a9e5f9dd35d5f3dd210f8f30f3
Linh Vo <linh.h.vo@ibm.com@Linhs-MacBook-Pro.local>
2018-04-02 13:46:08 -0500

    set away back

--- simplisafe/urls.py
+++ simplisafe/urls.py
@@ -6,6 +6,6 @@ admin.autodiscover()
 
 urlpatterns = [url(r'^$', core.views.home, name='home'),
                url(r'^admin/', include(admin.site.urls)),
-               # url(r'^simplisafe/away', core.views.simplisafe_away, name='simplisafe_away'),
+               url(r'^simplisafe/away', core.views.simplisafe_away, name='simplisafe_away'),
                url(r'^vacation/', core.views.set_vacation, name='vacation'),
                ]
