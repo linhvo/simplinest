bca6bac95bc82efff39ef62bab24e75f8916218c
linhvo <hailinh.vo@gmail.com>
2019-10-28 21:43:25 -0500

    fix bug with vacation mode

--- simplisafe/management/commands/turn_on_alarm.py
+++ simplisafe/management/commands/turn_on_alarm.py
@@ -1,7 +1,7 @@
 import logging
 from django.core.management import BaseCommand
 
-from core.models import Device
+from core.models import Device, NestAuth
 from core.views import set_simplisafe_state
 
 logger = logging.getLogger(__name__)
@@ -11,8 +11,10 @@ class Command(BaseCommand):
     help = 'Turn on alarm at 11:30'
 
     def handle(self, *args, **options):
-        if Device.vacation_mode:
-            print('Vacation')
+        auth = NestAuth.objects.first()
+        device = Device.objects.get(nest_auth=auth)
+        if device.vacation_mode:
+            print('In vacation mode, do not change state')
             return
 
         print('Turning on alarm')
